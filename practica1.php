6390a7262989719e739539638043cd17fe2e0f06
vagrant <vagrant@localhost.localdomain>
2016-03-09 17:47:54 +0000

    agrega primer archivo y modifica el readme.txt

---
+++ practica1.php
@@ -0,0 +1,27 @@
+<?php
+echo "WORLD \n";
+
+$boolen = true;
+$boolen = false;
+
+$int = 2;
+$int1 = 12;
+
+$float = 1.0987;
+$float = 292.2222;
+$float = 6353.0393;
+
+unset($float);
+
+var_dump($float);
+var_dump($int1);
+
+$sum = 1+1;
+$res = 1-10;
+$div = 2/4;
+$mult = 3*6;
+
+var_dump($res);
+var_dump($div);
+
+?>
