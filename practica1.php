08b892ca777fa473b71690afb441b8310802a51e
vagrant <vagrant@localhost.localdomain>
2016-03-09 18:20:55 +0000

    finaliza la practica 1

--- practica1.php
+++ practica1.php
@@ -13,7 +13,7 @@ $float = 6353.0393;
 
 unset($float);
 
-var_dump($float);
+//var_dump($float);
 var_dump($int1);
 
 $sum = 1+1;
@@ -23,5 +23,41 @@ $mult = 3*6;
 
 var_dump($res);
 var_dump($div);
+
+
+$num = 0;
+$num += 1;
+echo $num++;
+echo ++$num;
+
+$num -= 2;
+//$num /= $float;
+
+//string
+$num = 5;
+$varString = '$num \n';
+echo $varString;
+$varString = "el valor de la variable es = $num \n";
+echo $varString;
+
+$otroString = "otra forma de imprimir una variable {$num}";
+echo $otroString;
+$otroString = "otra forma de imprimir una variable $${num}";
+echo $otroString;
+
+//multi string
+$multiString = <<<'END'
+HOLA ESTE ES UN CURSO DE PHP NTERMEDIO
+STRING 
+END;
+
+echo $multiString;
+echo 'este es un'.'String';
+echo 'multiple','parametros','en el echo';
+
+//constantes
+define("_FOO", "HOLA");
+echo _FOO;
+
 
 ?>
